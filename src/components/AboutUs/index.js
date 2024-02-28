3b503766c37da7e61119df5527c9712dd1f1a317
Jenelle Holloway <jholloway@griddynamics.com>
2024-02-28 14:38:23 -0500

    add styling to about section

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import AboutUsCard from "../AboutUsCard";
+import "./index.css";
 
 const AboutUs = () => {
   return (
     <div className="parent-div">
-      <div>
+      <div className="about-container content-container">
         <AboutUsCard />
         <AboutUsCard count={2} />
       </div>
