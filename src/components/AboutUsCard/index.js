9c590adf72d94641559ed78c4fa5020c842f0a80
Jenelle Holloway <jholloway@griddynamics.com>
2024-02-28 14:32:40 -0500

    add styling to About Us Card

--- src/components/AboutUsCard/index.js
+++ src/components/AboutUsCard/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 import aboutus from "../../images/aboutus.png";
+import "./index.css";
 
 const AboutUsCard = ({ count }) => {
   return (
-    <div>
-      <img src={aboutus} alt="section of a green bush" />
-      <div>
-        <h4>About Us {count}</h4>
-        <p>
+    <div className="about-us-card">
+      <img
+        src={aboutus}
+        alt="section of a green bush"
+        className="about-us-img"
+      />
+      <div className="about-content">
+        <h4 className="about-us-title">About Us {count}</h4>
+        <p className="about-paragraph">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
           tincidunt posuere eu risus cursus. Senectus sed maecenas viverra
           tempus at. Nulla aliquam ac diam amet, sagittis, ut ultricies. Urna
@@ -17,7 +22,9 @@ const AboutUsCard = ({ count }) => {
           amet, consectetur adipiscing elit. Eleifend tincidunt posuere eu risus
           cursus.
         </p>
-        <button>Learn more about</button>
+        <button className="about-btn">
+          Learn more about <span class="fa fa-long-arrow-right"></span>
+        </button>
       </div>
     </div>
   );
