611ff90db4efeaeb3eea97b6d0e24a85af816e42
Jenelle Holloway <jholloway@griddynamics.com>
2024-02-29 11:47:33 -0500

    add styling to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import footericons from "../../images/footericons.png";
 
+import "./index.css";
+
 const Footer = () => {
   return (
     <footer className="parent-div">
       <div className="footer-container content-container">
         <div className="footer-form">
-          <h4>PlantMe</h4>
-          <p>
+          <h4 className="company-name">PlantMe</h4>
+          <p className="subscription-txt">
             Subscribe and be the first to know about our news and promotions.
           </p>
-          <input type="text" placeholder="example@gmail.com" />
-          <button>Subscribe</button>
+          <input
+            type="text"
+            placeholder="example@gmail.com"
+            id="footer-input"
+          />
+          <button className="subscribe-btn">Subscribe</button>
         </div>
         <nav className="footer-nav">
           <button>About Us</button>
@@ -21,9 +27,13 @@ const Footer = () => {
         </nav>
         <div className="footer-contact">
           <p>Contacts</p>
-          <p>+38 063 072 0121</p>
-          <p>plantme.store@gmail.com</p>
-          <img src={footericons} alt="visa and mastercard logos" />
+          <p className="footer-contact-details">+38 063 072 0121</p>
+          <p className="footer-contact-details">plantme.store@gmail.com</p>
+          <img
+            src={footericons}
+            alt="visa and mastercard logos"
+            className="footer-icons"
+          />
         </div>
       </div>
     </footer>
